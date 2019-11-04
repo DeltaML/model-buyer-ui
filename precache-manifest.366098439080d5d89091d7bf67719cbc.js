e801c8c967c8906e9a3d4ee3db9aa01aea236df4
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-11-04 15:38:32 +0000

    Deploy DeltaML/model-buyer-ui to github.com/DeltaML/model-buyer-ui.git:gh-pages

---
+++ precache-manifest.366098439080d5d89091d7bf67719cbc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "652b65b883238fe22340",
+    "url": "/static/js/main.652b65b8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "c48e6ee199ba71a322d9",
+    "url": "/static/js/2.c48e6ee1.chunk.js"
+  },
+  {
+    "revision": "a0185b043dee68a72aebffdcc939e21e",
+    "url": "/static/media/logo.a0185b04.svg"
+  },
+  {
+    "revision": "9ee11814bb3ed16c7c0471cf75f1ae62",
+    "url": "/index.html"
+  }
+];
